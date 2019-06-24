bb21c7ff259e6b8bcf3704c19e61fe37db53a2de
Andreas Angourakis <andros.spica@gmail.com>
2019-06-25 00:10:18 +0100

    expanded RenderBook.R, allowing pdf version

--- RenderBook.R
+++ RenderBook.R
@@ -1,5 +1,10 @@
-# create the nojeckyll file (once)
+# visit https://bookdown.org/yihui/bookdown for more options
+
+# create the nojeckyll file (once, if docs/ is empty)
 #file.create('docs/.nojekyll')
+
+# Go to 'index.Rmd' and make sure that 'params: ispdf: FALSE' 
+# at the end of the YAML header
 
 bookdown::render_book("index.Rmd", 
                       output_format = bookdown::gitbook(),
@@ -8,3 +13,24 @@ bookdown::render_book("index.Rmd",
                       output_dir = "docs", 
                       new_session = NA, preview = FALSE, 
                       encoding = "UTF-8", config_file = "_bookdown.yml")
+
+# It is posible to obtain the pdf version using the following code OR 
+# by accessing the save>pdf option in the html gitbook version generated above.
+
+# if MikTex is installed: 
+# devtools::install_github('yihui/tinytex')
+
+# Go to 'index.Rmd' and make sure that 'params: ispdf: TRUE' 
+# at the end of the YAML header
+
+bookdown::render_book("index.Rmd",
+                      output_format = bookdown::pdf_book(),
+                      clean = TRUE,
+                      envir = parent.frame(), clean_envir = !interactive(),
+                      output_dir = "pdf",
+                      new_session = NA, preview = FALSE,
+                      encoding = "UTF-8", config_file = "_bookdown.yml")
+
+# to preview a single chapter
+
+bookdown::preview_chapter("5_randomForest.Rmd")
